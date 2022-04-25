2e510b3b0554fc39fd03f181554b4ad6c3057a48
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-04-25 15:21:57 +1000

    Added a sprite structure.

---
+++ include/sprite.h
@@ -0,0 +1,16 @@
+#include "external.h"
+#include "texture.h"
+
+#ifndef SPRITE_H
+#define SPRITE_H
+
+struct Sprite {
+    Texture* texture;
+    vec2 texcoords[4];
+    vec2 size;    
+};
+typedef struct Sprite Sprite;
+
+void Sprite_Init(Sprite* s, Texture* texture, vec2 texcoords[4], vec2 size);
+
+#endif
---
+++ src/sprite.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "sprite.h"
+
+void Sprite_Init(Sprite* s, Texture* texture, vec2 texcoords[4], vec2 size) {
+    s->texture = texture;
+    glm_vec2_copy(size, s->size);
+    memcpy(s->texcoords, texcoords, sizeof(s->texcoords));
+}
