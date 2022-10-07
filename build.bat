6ee3b91cd2fc399344208255688bb695db99d28a
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-10-07 15:53:26 +1100

    Improved build script.

--- build.bat
+++ build.bat
@@ -2,3 +2,6 @@
 git submodule update --init --recursive
 cmake . -G "MinGW Makefiles"
 mingw32-make
+if not exist build (mkdir build > NUL)
+xcopy /s /F /Q /y assets\ build\assets\ > NUL
+xcopy /F /Q /y waffle.exe build\waffle.exe > NUL
