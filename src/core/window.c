d15aef44836a4188185a1a005c67629cc1bec89d
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-10-17 15:22:01 +1100

    Starting a clean slate with the immediate mode guis

--- src/core/window.c
+++ src/core/window.c
@@ -3,19 +3,6 @@
 #include "external.h"
 #include "window.h"
 
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_IMPLEMENTATION
-#define NK_GLFW_GL3_IMPLEMENTATION
-#define NK_KEYSTATE_BASED_INPUT
-#include "nuklear.h"
-#include "nuklear_glfw_gl3.h"
-
 #include "cameracontroller.h"
 
 #define MAX_VERTEX_BUFFER 512 * 1024
@@ -23,12 +10,6 @@
 
 GLFWwindow* window;
 Scene scene;
-
-/*
-struct nk_glfw glfw = {0};
-struct nk_context* ctx;
-struct nk_colorf bg;
-*/
 
 FrameBuffer entityTexture;
 Shader* defaultShader;
@@ -78,13 +59,6 @@ int Window_Init() {
 
     //Load GLAD so it configures OpenGL
 	gladLoadGL();
-
-    /*
-    ctx = nk_glfw3_init(&glfw, window, NK_GLFW3_INSTALL_CALLBACKS);
-    struct nk_font_atlas *atlas;
-    nk_glfw3_font_stash_begin(&glfw, &atlas);
-    nk_glfw3_font_stash_end(&glfw);
-    */
 
     // Set the window configuration.
     //Window_SetFullscreenWindowed();
@@ -146,45 +120,6 @@ void Window_Loop() {
             
             Renderer_BindShader(defaultShader);
             Scene_Render(&scene);
-
-            /*
-            nk_glfw3_new_frame(&glfw);
-            if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
-                NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-                NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE)) {
-                
-                enum {EASY, HARD};
-                static int op = EASY;
-                static int property = 20;
-                nk_layout_row_static(ctx, 30, 80, 1);
-                if (nk_button_label(ctx, "button")) {
-                    fprintf(stdout, "button pressed\n");
-                }
-                    
-                nk_layout_row_dynamic(ctx, 30, 2);
-                if (nk_option_label(ctx, "easy", op == EASY)) {op = EASY;}
-                if (nk_option_label(ctx, "hard", op == HARD)) {op = HARD;}
-
-                nk_layout_row_dynamic(ctx, 25, 1);
-                nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-                nk_layout_row_dynamic(ctx, 20, 1);
-                nk_label(ctx, "background:", NK_TEXT_LEFT);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
-                    nk_layout_row_dynamic(ctx, 120, 1);
-                    bg = nk_color_picker(ctx, bg, NK_RGBA);
-                    nk_layout_row_dynamic(ctx, 25, 1);
-                    bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
-                    bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
-                    bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
-                    bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
-                    nk_combo_end(ctx);
-                }
-            }
-            nk_end(ctx);
-            nk_glfw3_render(&glfw, NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
-            */
 
         }
 
