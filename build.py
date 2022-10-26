2e161e4edca8d0af411c66d6d1bc742abc5a3027
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-10-26 23:12:54 +1100

    Removed unused lines from build script.

--- build.py
+++ build.py
@@ -51,7 +51,6 @@ add_executable(${project_name} ${source_files})
 target_include_directories(${project_name} PUBLIC glfw)
 target_include_directories(${project_name} PUBLIC glad)
 target_include_directories(${project_name} PUBLIC cglm)
-target_include_directories(${project_name} PUBLIC chipmunk)
 
 if (WIN32)
 find_library(pthread NAME pthread)
