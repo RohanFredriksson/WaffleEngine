25e58b9a03c1a517d159e9e284c212aadb8121dd
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-10-30 20:56:52 +1100

    Finished implementing the box2d point intersection.

--- src/physics2d/rigidbody/intersection.c
+++ src/physics2d/rigidbody/intersection.c
@@ -1,4 +1,5 @@
 #include "intersection.h"
+#include "wmath.h"
 
 bool Intersection_PointOnLine(vec2 point, Line2D line) {
     float dy = line.to[1] - line.from[1];
@@ -14,13 +15,25 @@ bool Intersection_PointInCircle(vec2 point, Circle circle) {
 }
 
 bool Intersection_PointInAABB2D(vec2 point, AABB2D box) {
+
     vec2 min;
     vec2 max;
     AABB2D_GetMin(&box, min);
     AABB2D_GetMax(&box, max);
     return point[0] >= min[0] && point[0] <= max[0] && point[1] >= min[1] && point[1] <= max[1];
+
 }
 
 bool Intersection_PointInBox2D(vec2 point, Box2D box) {
-    return 0;
+    
+    vec2 pointLocalBoxSpace;
+    glm_vec2_copy(point, pointLocalBoxSpace);
+    WMath_Rotate(pointLocalBoxSpace, box.rigidbody->rotation, box.rigidbody->pos);
+
+    vec2 min;
+    vec2 max;
+    Box2D_GetMin(&box, min);
+    Box2D_GetMax(&box, max);
+    return pointLocalBoxSpace[0] >= min[0] && pointLocalBoxSpace[0] <= max[0] && pointLocalBoxSpace[1] >= min[1] && pointLocalBoxSpace[1] <= max[1];
+
 }
