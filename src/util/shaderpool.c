332d3c7a5385a20c5adb79c40ce3bba8a23970f8
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-11-18 14:31:55 +1100

    Shader pool now uses a hashmap.

--- src/util/shaderpool.c
+++ src/util/shaderpool.c
@@ -1,73 +1,77 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "external.h"
 #include "shader.h"
 #include "shaderpool.h"
 
-#define INITIAL_SHADERPOOL_SIZE 16
+static HashMap pool;
 
-Shader** ShaderPool_Pool;
-size_t ShaderPool_Size;
-size_t ShaderPool_Length;
+struct ShaderHash {
+    uint64_t vertex;
+    uint64_t fragment;
+};
+typedef struct ShaderHash ShaderHash;
+
+ShaderHash ShaderPool_Hash(const char* vertex, const char* fragment) {
+
+    ShaderHash result;
+    result.vertex = OAAT(vertex);
+    result.fragment = OAAT(fragment);
+    return result;
+
+}
 
 void ShaderPool_Init() {
-    ShaderPool_Pool = (Shader**) malloc(INITIAL_SHADERPOOL_SIZE * sizeof(Shader*));
-    ShaderPool_Size = INITIAL_SHADERPOOL_SIZE;
-    ShaderPool_Length = 0;
+    HashMap_Init(&pool, sizeof(ShaderHash), sizeof(Shader*));
 }
 
 void ShaderPool_Clear() {
 
     // Free all shader data.
-    for (int i = 0; i < ShaderPool_Length; i++) {
-        Shader_Free(ShaderPool_Pool[i]);
-        free(ShaderPool_Pool[i]);
+    KeyValue* current = HashMap_Elements(&pool);
+    while (current != NULL) {
+        Shader* shader = (Shader*) current->value;
+        Shader_Free(shader);
+        current = current->next;
     }
-
-    // Set the length of the shader pool list to 0.
-    ShaderPool_Length = 0;
+    HashMap_Clear(&pool);
 
 }
 
 void ShaderPool_Free() {
-    
-    // Free all shader data.
-    for (int i = 0; i < ShaderPool_Length; i++) {
-        Shader_Free(ShaderPool_Pool[i]);
-        free(ShaderPool_Pool[i]);
-    }
 
-    // Free the shader pool.
-    free(ShaderPool_Pool);
+    // Free all shader data.
+    KeyValue* current = HashMap_Elements(&pool);
+    while (current != NULL) {
+        Shader* shader = (Shader*) current->value;
+        Shader_Free(shader);
+        current = current->next;
+    }
+    HashMap_Free(&pool);
 
 }
 
 Shader* ShaderPool_Get(const char* vertexFilepath, const char* fragmentFilepath) {
 
-    // If the shader already exists, return the shader.
-    for (int i = 0; i < ShaderPool_Length; i++) {
-        Shader* currentShader = ShaderPool_Pool[i];
-        if (strcmp(currentShader->vertexFilepath, vertexFilepath) == 0 && 
-            strcmp(currentShader->fragmentFilepath, fragmentFilepath) == 0) {
-            return currentShader;
-        }
-    }
+    // Create the ShaderHash object.
+    ShaderHash hash = ShaderPool_Hash(vertexFilepath, fragmentFilepath);
+    Shader* shader;
 
-    // The shader does not exist. Add it.
-    // If the shaderpool is not big enough, allocate more memory.
-    if (ShaderPool_Length >= ShaderPool_Size) {
-        ShaderPool_Pool = (Shader**) realloc(ShaderPool_Pool, ShaderPool_Size * 2 * sizeof(Shader*));
-        ShaderPool_Size = ShaderPool_Size * 2;
+    // If the shader already exists, return the shader.
+    if (HashMap_Get(&pool, &hash, &shader)) {
+        return shader;
     }
 
     // Initialise and compile the shader.
-    Shader* newShader = (Shader*) malloc(sizeof(Shader));
-    Shader_Init(newShader, vertexFilepath, fragmentFilepath);
-    Shader_Compile(newShader);
-    ShaderPool_Pool[ShaderPool_Length] = newShader;
-    ShaderPool_Length++;
+    shader = malloc(sizeof(Shader));
+    Shader_Init(shader, vertexFilepath, fragmentFilepath);
+    Shader_Compile(shader);
+    
+    // Add the shader to the pool.
+    HashMap_Put(&pool, &hash, &shader);
 
     // Return the new shader.
-    return newShader;
+    return shader;
 
 }
